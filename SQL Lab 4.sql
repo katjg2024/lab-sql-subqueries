9dc465baf0713641df27e47774b92cff491c3063
katjg2024 <168486302+katjg2024@users.noreply.github.com>
2024-05-22 00:20:22 -0400

    Attempted 2 bonuses

--- SQL Lab 4.sql
+++ SQL Lab 4.sql
@@ -42,3 +42,66 @@ WHERE fa.film_id = (SELECT film_id
 		WHERE title = 'Alone Trip');
 
 
+-- Sales have been lagging among young families, and you want to target family movies for a promotion. 
+-- Identify all movies categorized as family films.
+select name
+from category;
+
+-- category id, film id, category name, film name
+SELECT title
+from film as f
+JOIN film_category as fc
+ON f.film_id = fc.film_id
+JOIN category as c
+ON c.category_id = fc.category_id
+WHERE f.title IN
+	(SELECT name
+	FROM category as c
+	WHERE name = 'Family');
+
+SELECT title
+FROM film
+WHERE film_id IN (
+    SELECT film_id
+    FROM film_category
+    WHERE category_id = (
+        SELECT category_id
+        FROM category
+        WHERE name = 'Family'
+    )
+);
+
+
+-- Retrieve the name and email of customers from Canada using both subqueries and joins. 
+-- To use joins, you will need to identify the relevant tables and their primary and foreign keys.
+
+#name and email in customer table
+#customer table connects to address table through address_id
+#address table has city_id
+#city_id is in city table which connects to country table through country id
+#country table has country where i can find Canada
+
+SELECT first_name, last_name, email
+FROM customer
+WHERE address_id IN 
+ (SELECT address_id
+  FROM address
+  WHERE city_id IN
+	(SELECT city_id
+    FROM city
+    WHERE country_id =
+		(SELECT country_id
+		FROM country
+		WHERE country = 'Canada')));
+        
+
+-- Determine which films were starred by the most prolific actor in the Sakila database. 
+-- A prolific actor is defined as the actor who has acted in the most number of films. 
+-- First, you will need to find the most prolific actor and then use that actor_id to find the different films that he or she starred in.
+
+
+
+
+
+
+
